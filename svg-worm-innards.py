0f6007d6348c69328e354d035f2dc33e3771ff52
Matt Leffler <matthewjleffler@gmail.com>
2022-12-29 17:00:12 -0500

    Worm
    - Added worm and worm-innards

---
+++ svg-worm-innards.py
@@ -0,0 +1,53 @@
+import lib
+import random
+import math
+
+
+def loop():
+  # Border
+  lib.rect(lib.svg_safe.x, lib.svg_safe.y, lib.svg_safe.w, lib.svg_safe.h, "red")
+
+  spread_border = 20
+  border_x = 10
+  border_y = 0
+  spread_x = 200
+  spread_y = 115
+  row_2_offset = 190
+
+  for row in range(0, 6):
+    origin_y = spread_border + spread_y * row
+
+    for col in range(0, 5):
+      stack_count = 20
+
+      origin_x = spread_border + spread_x * col
+
+      if row % 2 == 1:
+        origin_x += row_2_offset
+
+      for i in range(0, stack_count + 1):
+        percent = i / stack_count
+        pi_half_percent = percent * math.pi * .25
+
+        size = 10
+        half = size / 2
+
+        if row % 2 == 0:
+          x = lib.svg_safe.x + border_x + origin_x + math.sin(pi_half_percent) * 10 * i
+          y = lib.svg_safe.y + border_y + origin_y + math.cos(pi_half_percent) * 10 * i
+        else:
+          x = lib.svg_safe.x + border_x + origin_x - math.sin(pi_half_percent) * 10 * i
+          y = lib.svg_safe.x + border_y + origin_y + math.cos(pi_half_percent) * 10 * i
+
+        lib.circ(x, y, half)
+
+
+if __name__ == "__main__":
+  lib.main(
+    "worm-innards",
+    True,
+    0,
+    lib.SvgSize.Size9x12,
+    loop
+  )
+
