13b718531e8aa1cba9a26458198a51e618ba6584
Matt Leffler <matthewjleffler@gmail.com>
2022-12-31 12:42:44 -0500

    More organic worm generation

--- svg-long-worm.py
+++ svg-long-worm.py
@@ -19,29 +19,35 @@ highlight_end_points = 10
 highlight_end_point_radius = 5
 
 # Init variables
+rough_points = [] # Initial guide points
 points = [] # Line points
 centers = [] # Line centers for bezier curves
 positions = [] # Final render positions
 
 def init():
-  global points, centers, positions
+  global rough_points, points, centers, positions
+  rough_points = []
   points = []
   centers = []
   positions = []
 
 
-# Clamp line points (pre-curve)
-def clamp_line(value):
+# Round line points (pre-curve)
+def round_line(value):
   if clamp_point == 0:
     return value
   return round(value / clamp_point, 0) * clamp_point
 
 
-# Clamp render positions
-def clamp(value):
+# Round render positions
+def round_position(value):
   if clamp_size == 0:
     return value
   return round(value / clamp_size, 0) * clamp_size
+
+
+def clamp(val, minv, maxv):
+  return max(min(val, maxv), minv)
 
 
 class Position:
@@ -75,20 +81,20 @@ def add_points_along_line(p0, p1, size, next_size):
     step_size = lib.ease_in_out_quad(copy_len, size, next_size - size, length)
     # step_size = lib.lerp(size, next_size, copy_len / length)
     if circle:
-      add_nondup_position(clamp(p0.x + copy.x), clamp(p0.y + copy.y), step_size, positions)
+      add_nondup_position(round_position(p0.x + copy.x), round_position(p0.y + copy.y), step_size, positions)
     else:
       x = p0.x + copy.x - step_size
       y = p0.y + copy.y - step_size
-      add_nondup_position(clamp(x), clamp(y), step_size * 2, positions)
+      add_nondup_position(round_position(x), round_position(y), step_size * 2, positions)
     copy = vector.multiply_copy(copy_len + step_dist)
     copy_len = copy.length()
 
   if circle:
-    add_nondup_position(clamp(p1.x), clamp(p1.y), next_size, positions)
+    add_nondup_position(round_position(p1.x), round_position(p1.y), next_size, positions)
   else:
     x = p1.x - next_size
     y = p1.y - next_size
-    add_nondup_position(clamp(x), clamp(y), next_size * 2, positions)
+    add_nondup_position(round_position(x), round_position(y), next_size * 2, positions)
 
 
 def add_points_along_curve(p0, p1, control, size, next_size):
@@ -103,19 +109,19 @@ def add_points_along_curve(p0, p1, control, size, next_size):
     step_size = lib.ease_in_out_quad(t, size, next_size - size, 1)
 
     if circle:
-      add_nondup_position(clamp(point.x), clamp(point.y), step_size, positions)
+      add_nondup_position(round_position(point.x), round_position(point.y), step_size, positions)
     else:
       x = point.x - step_size
       y = point.y - step_size
-      add_nondup_position(clamp(x), clamp(y), step_size * 2, positions)
+      add_nondup_position(round_position(x), round_position(y), step_size * 2, positions)
 
   # Finish with final point
   if circle:
-    add_nondup_position(clamp(p1.x), clamp(p1.y), next_size, positions)
+    add_nondup_position(round_position(p1.x), round_position(p1.y), next_size, positions)
   else:
     x = p1.x - next_size
     y = p1.y - next_size
-    add_nondup_position(clamp(x), clamp(y), next_size * 2, positions)
+    add_nondup_position(round_position(x), round_position(y), next_size * 2, positions)
 
 
 def draw_worm_path(draw):
@@ -137,11 +143,11 @@ def draw_worm_path(draw):
 
   # Draw start circle
   if circle:
-    add_nondup_position(clamp(points[0].x), clamp(points[0].y), size_end, positions)
+    add_nondup_position(round_position(points[0].x), round_position(points[0].y), size_end, positions)
   else:
     x = points[0].x - size_end
     y = points[0].y - size_end
-    add_nondup_position(clamp(x), clamp(y), size_end * 2, positions)
+    add_nondup_position(round_position(x), round_position(y), size_end * 2, positions)
   size = size_end
   next_size = 0
 
@@ -220,47 +226,105 @@ def draw_worm_highlights(draw):
 
 
 def loop(draw_worm, draw_highlight):
-  # lib.border()
+  lib.border()
 
   init()
 
   # Variables
-  down = True
-  padding = 100
-  jump_x_min = 10
-  jump_x_max = 20
-  jump_y_min = 30
-  jump_y_max = 200
+  peaks = 5
+  shuffle_large_max_x = 80
+  shuffle_large_max_y = 100
+  shuffle_small_max_x = 30
+  shuffle_small_max_y = 30
+  top_first = random.randint(0, 1) == 0
+  rough_subdivisions_min = 1
+  rough_subdivisions_max = 10
 
-  # Pick random lines
+  # Build outline
+  padding = 100
   top = lib.svg_safe.y + padding
   bottom = lib.svg_safe.bottom() - padding
-  last_x = clamp_line(lib.svg_safe.x + padding)
-  safe_third = round(lib.svg_safe.h / 3, 0)
-  last_y = clamp_line(random.randrange(lib.svg_safe.y + safe_third, lib.svg_safe.bottom() - safe_third))
-  points.append(lib.Point(last_x, last_y))
+  center_y = lib.svg_full.h / 2
+  left = lib.svg_safe.x + padding
+  right = lib.svg_safe.right() - padding
+  width = right - left
 
-  while last_x < lib.svg_safe.right() - padding:
-    last_x += random.randrange(jump_x_min, jump_x_max)
-    last_x = clamp_line(last_x)
-    if down:
-      new_y = last_y + random.randrange(jump_y_min, jump_y_max)
-      if new_y > bottom:
-        down = False
-        new_y = last_y
-    else:
-      new_y = last_y - random.randrange(jump_y_min, jump_y_max)
-      if new_y < top:
-        down = True
-        new_y = last_y
+  # Subdivide space
+  space = width / (peaks + 1)
 
-    last_y = clamp_line(new_y)
-    lib.add_nondup_point(last_x, last_y, points)
+  # Pick direction of motion
+  if top_first:
+    first = top
+    second = bottom
+  else:
+    first = bottom
+    second = top
 
-  # Create straight path and generate centerpoints
-  # TODO move centerpoints up into previous loop
-  last = points[0]
+  # Pick rough points
+  rough_points.append(lib.Point(left, center_y))
+  for i in range(0, peaks):
+    rough_points.append(lib.Point(left + space * (i + 1), first))
+    rough_points.append(lib.Point(left + space * (i + 1), second))
+  rough_points.append(lib.Point(right, center_y))
+
+  # Shuffle rough points
+  for point in rough_points:
+    change_x =  random.randrange(-shuffle_large_max_x, shuffle_large_max_x)
+    change_y = random.randrange(-shuffle_large_max_y, shuffle_large_max_y)
+    point.x = clamp(point.x + change_x, left, right)
+    point.y = clamp(point.y + change_y, top, bottom)
+
+  # Draw rough points
+  # last = rough_points[0]
+  # path = "M{} {}".format(last.x, last.y)
+  # for i in range(1, len(rough_points)):
+  #   point = rough_points[i]
+  #   path += " L{} {}".format(point.x, point.y)
+  # lib.path(path)
+
+  # Subdivide rough path
+  last = rough_points[0]
   path = "M{} {}".format(last.x, last.y)
+  points.append(rough_points[0])
+
+  for i in range(1, len(rough_points)):
+    point = rough_points[i]
+    vector = point.subtract(last)
+    length = vector.length()
+
+    subdivisions = random.randint(rough_subdivisions_min, rough_subdivisions_max)
+    sub_length = length / subdivisions
+    vector.normalize()
+    vector.multiply(sub_length)
+
+    x = last.x
+    y = last.y
+    points.append(lib.Point(x, y))
+    for _ in range(0, subdivisions):
+      x += vector.x
+      y += vector.y
+      points.append(lib.Point(x, y))
+      # lib.circ(x, y, 5)
+
+    last = point
+
+  # Shuffle subdivided path
+  for point in points:
+    change_x = random.randrange(-shuffle_small_max_x, shuffle_small_max_x)
+    change_y = random.randrange(-shuffle_small_max_y, shuffle_small_max_y)
+    point.x += change_x
+    point.y += change_y
+
+  # Draw subdivided points
+  # last = points[0]
+  # path = "M{} {}".format(last.x, last.y)
+  # for i in range(1, len(points)):
+  #   point = points[i]
+  #   path += " L{} {}".format(point.x, point.y)
+  # lib.path(path)
+
+  # Generate centerpoints
+  last = points[0]
   for i in range(1, len(points)):
     point = points[i]
 
@@ -274,12 +338,9 @@ def loop(draw_worm, draw_highlight):
     center_y = last.y + vector.y
     centers.append(lib.Point(center_x, center_y))
 
-    path += " L{} {}".format(point.x, point.y)
+    # lib.circ(center_x, center_y, 5)
 
     last = point
-
-  # Draw straight line
-  # lib.path(path)
 
   draw_worm_path(draw_worm)
   draw_worm_highlights(draw_highlight)
