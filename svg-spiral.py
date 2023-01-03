fa2071fdd72d7ef5d7c063bccd4464fdd85f868b
Matt Leffler <matthewjleffler@gmail.com>
2023-01-03 12:54:53 -0500

    Update spiral to be a cleaner image

--- svg-spiral.py
+++ svg-spiral.py
@@ -3,12 +3,19 @@ import math
 import random
 import path
 from typing import List
+from enum import IntEnum
 
-# TODO different borders
 # TODO wiggle the origins
 
-rotate_range = math.pi / 30
-subdivisions = 20
+class BorderType(IntEnum):
+  Empty = 0
+  Circles = 1
+  Starburst = 2
+
+  End = 3
+
+rotate_range = math.pi / 10
+ring_pad = 5
 
 def point(center_x:float, center_y:float, rad:float, dist:float) -> lib.Point:
   return lib.Point(round(cos(center_x, rad, dist), 2),
@@ -20,37 +27,70 @@ def cos(center:float, rads:float, dist:float) -> float:
 def sin(center:float, rads:float, dist:float) -> float:
   return center + math.sin(rads) * dist
 
-def draw_circle(x, y, rays, min_dist, max_dist, points):
+def draw_circle(x:float, y:float, rays:int, min_dist:float, max_dist:float, points:List[lib.Point]):
+  # Create original rough line
+  rough_points: List[lib.Point] = []
+  rough_points.append(point(x, y, 0, min_dist))
+  rough_points.append(point(x, y, 0, max_dist))
+
+  # Draw rough line
+  # path.draw_point_circles(rough_points)
+  # path.draw_point_path(rough_points)
+
+  # Subdivide
+  subdivisions = math.floor(max_dist / 10)
+  if subdivisions % 2 == 1:
+    subdivisions += 1
+  subdivided = path.subdivide_point_path(rough_points, subdivisions, subdivisions, False)
+
+  # Adjust control points
+  for i in range(1, len(subdivided), 2):
+    sub = subdivided[i].subtract_floats(x, y)
+    rot = lib.rand_float(-rotate_range, rotate_range)
+    fine = sub.rotate(rot)
+    subdivided[i] = lib.Point(x + fine.x, y + fine.y)
+
+  # Draw subdivided line
+  # path.draw_point_circles(subdivided)
+  # path.draw_point_path(subdivided)
+
+  # Copy by number of rays
   for i in range(0, rays):
-    rough_points: List[lib.Point] = []
-    t = i / rays
-    rad = t * math.pi * 2
-    rough_points.append(point(x, y, rad, min_dist))
-    rough_points.append(point(x, y, rad, max_dist))
+    final: List[lib.Point] = []
 
-    # Draw rough line
-    # path.draw_point_circles(rough_points)
-    # path.draw_point_path(rough_points)
+    rot = (i / rays) * math.pi * 2
+    for sub in subdivided:
+      rotated = sub.subtract_floats(x, y)
+      rotated = rotated.rotate(rot)
+      final.append(lib.Point(x + rotated.x, y + rotated.y))
 
-    # Subdivide
-    fine_points = path.subdivide_point_path(rough_points, subdivisions, subdivisions, False)
+    # Draw rotated lines
+    # path.draw_point_circles(final)
+    # path.draw_point_path(final)
+    points.append(final)
 
-    # Adjust control points
-    for j in range(1, len(fine_points), 2):
-      fine = fine_points[j].subtract_floats(x, y)
-      rotation = lib.rand_float(-rotate_range, rotate_range)
-      fine = fine.rotate(rotation)
-      fine_points[j] = lib.Point(x + fine.x, y + fine.y)
-
-    # Add final content to render list
-    points.append(fine_points)
-
-    # Draw fine line
-    # path.draw_point_circles(fine_points)
-    # path.draw_point_path(fine_points)
+  # points.append(subdivided)
 
 
-def loop():
+def add_border(x, y, size_h, draw):
+  ring_count = random.randint(0, 3)
+  if draw:
+    for i in range(0, ring_count):
+      lib.circ(x, y, size_h + ring_pad + ring_pad * i)
+
+  ring_buffer = ring_pad + ring_pad * (ring_count + 1)
+
+  border_index = random.randint(0, int(BorderType.End) - 1)
+  border = BorderType(border_index)
+  if border == BorderType.Empty or not draw:
+    return
+  elif border == BorderType.Circles:
+    lib.ring_of_circles(math.floor(size_h / 3), x, y, size_h + ring_buffer, 5)
+  elif border == BorderType.Starburst:
+    lib.sunburst(math.floor(size_h / 3), x, y, size_h + ring_buffer, 5)
+
+
+def loop(draw_circles, draw_border):
   # lib.border()
 
   # Rough path
@@ -67,8 +107,8 @@ def loop():
   # Generate points and control points
   points: List[List[lib.Point]] = []
 
-  size_h = random.randint(80, 120)
-  size = size_h + 5
+  size_h = random.randint(40, 280)
+  size = size_h + 35
   size_d = size * 2
 
   col_max = math.floor(width / size_d)
@@ -77,6 +117,8 @@ def loop():
   offset_x = (lib.svg_safe.w - (col_max * size_d)) / 2
   offset_y = (lib.svg_safe.h - size / 2 - (row_max * size_d * .85)) / 2
 
+  # Calculate circles and draw borders
+  lib.open_group("stroke=\"blue\"")
   for row in range(0, row_max):
     y = offset_y + top + size + (size_d * .85) * row
     for col in range(0, col_max):
@@ -85,8 +127,9 @@ def loop():
         if col == col_max - 1:
           continue
         x += size
-      draw_circle(x, y, size_h, 10, size_h, points)
-      lib.circ(x, y, size_h)
+      draw_circle(x, y, math.floor(size_h / 2), 10, size_h, points)
+      add_border(x, y, size_h, draw_border)
+  lib.close_group()
 
   # Draw points
   for point_array in points:
@@ -99,13 +142,25 @@ def loop():
       control = point_array[i]
       end = point_array[i + 1]
       path += "Q{} {} {} {}".format(control.x, control.y, end.x, end.y)
-    lib.path(path)
+    if draw_circles:
+      lib.path(path)
 
+
+def loop_combined():
+  loop(True, True)
+
+def loop_circles():
+  loop(True, False)
+
+def loop_highlights():
+  loop(False, True)
 
 seed = 0
 test = True
 size = lib.SvgSize.Size9x12
 
 if __name__ == "__main__":
-  lib.main("spiral", test, seed, size, loop)
+  lib.main("spiral-combined", test, seed, size, loop_combined)
+  lib.main("spiral-circles", test, seed, size, loop_circles)
+  lib.main("spiral-highlights", test, seed, size, loop_highlights)
 
