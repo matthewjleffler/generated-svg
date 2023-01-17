370519ca63d0f8da75999ce92a9b7959fb9668dc
Matt Leffler <matthewjleffler@gmail.com>
2023-01-17 16:48:16 -0500

    Checkerboard Fill
    - Create several fill types, aligned horiz, aligned vert, filled (vertical)
    - Output as separate layers

--- lib_checkerboard.py
+++ lib_checkerboard.py
@@ -6,19 +6,20 @@ from math import *
 ### Checkerboard Design
 ###
 
-# TODO fill spaces
-
 _deg_to_rad = pi / 180
 
 class CheckerboardParams:
   def __init__(self) -> None:
-    self.draw = True
+    self.draw_lines = True
+    self.draw_aligned_vertical = True
+    self.draw_aligned_horizontal = True
+    self.draw_filled_checkers = True
     self.pad = 0
     self.size = RangeInt(10, 150)
-    self.skew_vert_degs = RangeFloat(-45, 45)
-    self.skew_horiz_degs = RangeFloat(-45, 45)
-    self.interior_space = 5
-    self.draw_aligned_interior = True
+    self.skew_vert_degs = RangeFloat(0, 45)
+    self.skew_horiz_degs = RangeFloat(0, 45)
+    self.interior_space_aligned = 5
+    self.interior_space_filled = 5
 
 def line(p1:Point, p2:Point):
   A = (p1.y - p2.y)
@@ -59,30 +60,127 @@ def _create_line_endpoints(
   lines:List):
 
   origin_point = Point(origin_x, origin_y)
-  vec = line(origin_point, origin_point.add_copy(vec))
+  vec_line = line(origin_point, origin_point.add_copy(vec))
 
-  end_point = intersection(vec, lines[0])
-  intersect_1 = intersection(vec, lines[1])
-  if intersect_1 is not None:
-    len_0 = end_point.subtract_copy(origin_point).length()
-    len_1 = intersect_1.subtract_copy(origin_point).length()
-    if len_1 < len_0:
-      end_point = intersect_1
-
-  # Cast backwards from end point to the top / left edge
-  origin_point = intersection(vec, lines[2])
-  intersect_3 = intersection(vec, lines[3])
-  if intersect_3 is not None:
-    len_2 = end_point.subtract_copy(origin_point).length()
-    len_3 = end_point.subtract_copy(intersect_3).length()
-    if len_3 < len_2:
-      origin_point = intersect_3
+  end_point = _pick_closest_intersection(origin_point, vec_line, [lines[0], lines[1]])
+  origin_point = _pick_closest_intersection(end_point, vec_line, [lines[2], lines[3]])
 
   if not pad_rect.contains(origin_point.x, origin_point.y) and not pad_rect.contains(end_point.x, end_point.y):
     # Out of bounds
     return None
 
   return (origin_point, end_point)
+
+def _pick_bottom_intersection(checker_bounds:Rect, intersections:List[Point]) -> Point:
+  max = -maxsize
+  result = None
+  for i in intersections:
+    if i is None:
+      continue
+    if not checker_bounds.contains(i.x, i.y):
+      # Not a valid result
+      continue
+    if i.y > max:
+      max = i.y
+      result = i
+  return result
+
+def _pick_top_intersection(checker_bounds:Rect, intersections:List[Point]) -> Point:
+  min = maxsize
+  result = None
+  for i in intersections:
+    if i is None:
+      continue
+    if not checker_bounds.contains(i.x, i.y):
+      # Not a valid result
+      continue
+    if i.y < min:
+      min = i.y
+      result = i
+  return result
+
+def _pick_closest_intersection(origin:Point, line, others) -> Point:
+  result = None
+  min = maxsize
+  for other in others:
+    point = intersection(line, other)
+    if point is None:
+      continue
+    dist = point.subtract_copy(origin)
+    length = dist.length()
+    if length < min:
+      min = length
+      result = point
+  return result
+
+def _create_fill(
+  vec:Point,
+  pad_rect:Rect,
+  checker, bounds, checker_fill:List[Point],
+  params:CheckerboardParams) -> List[Point]:
+
+  left = checker[0]
+  right = checker[1]
+  top = checker[2]
+  bottom = checker[3]
+  bounds_top = bounds[0]
+  bounds_bottom = bounds[1]
+
+  top_left = intersection(left, top)
+  top_right = intersection(right, top)
+  bottom_left = intersection(left, bottom)
+  bottom_right = intersection(right, bottom)
+
+  min_x = min(top_left.x, top_right.x, bottom_left.x, bottom_right.x)
+  max_x = max(top_left.x, top_right.x, bottom_left.x, bottom_right.x)
+  min_y = min(top_left.y, top_right.y, bottom_left.y, bottom_right.y)
+  max_y = max(top_left.y, top_right.y, bottom_left.y, bottom_right.y)
+
+  space_x = max_x - min_x
+  space_y = max_y - min_y
+  count = floor(space_x / params.interior_space_filled)
+  real_dist = space_x / count
+  valid_rect = Rect(min_x, min_y, space_x, space_y)
+
+  for i in range(1, count):
+    origin_x = min_x + real_dist * i
+    origin_y = min_y - 10
+
+    origin = Point(origin_x, origin_y)
+    stroke_line = line(origin, origin.add_copy(vec))
+
+    l_intersect = intersection(stroke_line, left)
+    r_intersect = intersection(stroke_line, right)
+    t_intersect = intersection(stroke_line, top)
+    b_intersect = intersection(stroke_line, bottom)
+    intersections = [l_intersect, r_intersect, t_intersect, b_intersect]
+
+    # Check validity as we go
+    start = _pick_top_intersection(valid_rect, intersections)
+    if start is None:
+      continue
+    end = _pick_bottom_intersection(valid_rect, intersections)
+    if end is None:
+      continue
+
+    # Out of bounds
+    if start.x < pad_rect.x or start.x > pad_rect.right() or end.y < pad_rect.y or start.y > pad_rect.bottom():
+      continue
+
+    # Test top and bottom intersections
+    top_intersection = intersection(stroke_line, bounds_top)
+    if top_intersection.y > start.y:
+      start = top_intersection
+
+    bottom_insection = intersection(stroke_line, bounds_bottom)
+    if bottom_insection.y < end.y:
+      end = bottom_insection
+
+    checker_fill.append(start)
+    checker_fill.append(end)
+
+    # draw_path(f"M{start.x} {start.y}L{end.x} {end.y}")
+
 
 
 def draw_checkerboard(params:CheckerboardParams, group:Group = None):
@@ -126,9 +224,13 @@ def draw_checkerboard(params:CheckerboardParams, group:Group = None):
   horiz_vec = Point(1, 0)
   horiz_vec = horiz_vec.rotate_copy(horiz_rads)
 
+  fill_vec = Point(0, 1)
+
   # Line points
   vert: List[Point] = []
+  vert_lines = []
   horiz: List[Point] = []
+  horiz_lines = []
 
   lines_vert = [line_bottom, line_right, line_top, line_left]
   if vert_degs > 0:
@@ -143,6 +245,11 @@ def draw_checkerboard(params:CheckerboardParams, group:Group = None):
     origin_x = pad_rect.x + i * size
     origin_y = pad_rect.y
 
+    # Store line for later
+    vert_line = line(Point(origin_x, origin_y), Point(origin_x + vert_vec.x, origin_y + vert_vec.y))
+    vert_lines.append(vert_line)
+
+    # Create rendering points
     points = _create_line_endpoints(origin_x, origin_y, vert_vec, pad_rect, lines_vert)
     if points is not None:
       vert.append(points[0])
@@ -152,29 +259,33 @@ def draw_checkerboard(params:CheckerboardParams, group:Group = None):
     origin_x = pad_rect.x
     origin_y = pad_rect.y + i * size
 
+    # Store line for later
+    horiz_line = line(Point(origin_x, origin_y), Point(origin_x + horiz_vec.x, origin_y + horiz_vec.y))
+    horiz_lines.append(horiz_line)
+
+    # Create rendering points
     points = _create_line_endpoints(origin_x, origin_y, horiz_vec, pad_rect, lines_horiz)
     if points is not None:
       horiz.append(points[0])
       horiz.append(points[1])
 
   # Draw primary lines
-  if params.draw:
+  if params.draw_lines:
     path = _create_point_path_alternating(vert)
     path += _create_point_path_alternating(horiz)
     draw_path(path, group)
 
   # Interior fill
-
-  # TODO pick out checkerboard shapes
-  fill_count_per = floor(size / params.interior_space)
+  fill_count_per = floor(size / params.interior_space_aligned)
   offset_space = size / fill_count_per
   fill_vert: List[Point] = []
   fill_horiz: List[Point] = []
 
-  # Step through and draw checkerboard fill
+  # Step through and create aligned fill
   for i in range(0, len(vert), 4):
     start = vert[i]
-    for j in range(0, fill_count_per):
+    # Create interior aligned fill
+    for j in range(1, fill_count_per):
       offset = j * offset_space
       points = _create_line_endpoints(start.x + offset, start.y, vert_vec, pad_rect, lines_vert)
       if points is not None:
@@ -183,22 +294,59 @@ def draw_checkerboard(params:CheckerboardParams, group:Group = None):
 
   for i in range(0, len(horiz), 4):
     start = horiz[i]
-    for j in range(0, fill_count_per):
+    # Create interior aligned fill
+    for j in range(1, fill_count_per):
       offset = j * offset_space
       points = _create_line_endpoints(start.x, start.y + offset, horiz_vec, pad_rect, lines_horiz)
       if points is not None:
         fill_horiz.append(points[0])
         fill_horiz.append(points[1])
 
-  # Draw interior lines
-  if params.draw_aligned_interior:
+  # Draw interior aligned lines
+  if params.draw_aligned_vertical:
     open_group("stroke=\"red\"")
     path = _create_point_path_alternating(fill_vert)
     draw_path(path)
     close_group()
 
+  if params.draw_aligned_horizontal:
     open_group("stroke=\"blue\"")
     path = _create_point_path_alternating(fill_horiz)
+    draw_path(path)
+    close_group()
+
+
+  # Step through and create checkerboard sets
+  checkers = []
+  checker_fill: List[Point] = []
+
+  # Add vertical checkers
+  for i in range(0, len(vert_lines) - 1, 2):
+    checker_left = vert_lines[i]
+    checker_right = vert_lines[i + 1]
+    for j in range(0, len(horiz_lines) - 1, 2):
+      checker_top = horiz_lines[j]
+      checker_bottom = horiz_lines[j + 1]
+      checkers.append((checker_left, checker_right, checker_top, checker_bottom))
+
+  # Add horizontal checkers
+  for i in range(1, len(horiz_lines) - 1, 2):
+    checker_top = horiz_lines[i]
+    checker_bottom = horiz_lines[i + 1]
+    for j in range(1, len(vert_lines) - 1, 2):
+      checker_left = vert_lines[j]
+      checker_right = vert_lines[j + 1]
+      checkers.append((checker_left, checker_right, checker_top, checker_bottom))
+
+  # Create checker fill points
+  top_bottom = [line_top, line_bottom]
+  for i in range(0, len(checkers)):
+    checker = checkers[i]
+    _create_fill(fill_vec, pad_rect, checker, top_bottom, checker_fill, params)
+
+  if params.draw_filled_checkers:
+    open_group("stroke=\"green\"", group)
+    path = _create_point_path_alternating(checker_fill)
     draw_path(path)
     close_group()
 
--- svg_checkerboard.py
+++ svg_checkerboard.py
@@ -5,13 +5,53 @@ class CheckerboardRunner(Runner):
   def __init__(self) -> None:
     super().__init__("checkerboard")
 
-  def loop(self):
+  def loop_combined(self):
     params = CheckerboardParams()
     draw_checkerboard(params)
     return params
 
+  def loop_main(self):
+    params = CheckerboardParams()
+    params.draw_lines = True
+    params.draw_aligned_vertical = False
+    params.draw_aligned_horizontal = False
+    params.draw_filled_checkers = False
+    draw_checkerboard(params)
+    return params
+
+  def loop_vert(self):
+    params = CheckerboardParams()
+    params.draw_lines = False
+    params.draw_aligned_vertical = True
+    params.draw_aligned_horizontal = False
+    params.draw_filled_checkers = False
+    draw_checkerboard(params)
+    return params
+
+  def loop_horiz(self):
+    params = CheckerboardParams()
+    params.draw_lines = False
+    params.draw_aligned_vertical = False
+    params.draw_aligned_horizontal = True
+    params.draw_filled_checkers = False
+    draw_checkerboard(params)
+    return params
+
+  def loop_fill(self):
+    params = CheckerboardParams()
+    params.draw_lines = False
+    params.draw_aligned_vertical = False
+    params.draw_aligned_horizontal = False
+    params.draw_filled_checkers = True
+    draw_checkerboard(params)
+    return params
+
   def run(self, test:bool, seed:int, size:SvgSize) -> int:
-    mainseed = main(self.dir, "main", test, seed, size, self.loop)
+    mainseed = main(self.dir, "combined", test, seed, size, self.loop_combined)
+    main(self.dir, "main", test, mainseed, size, self.loop_main)
+    main(self.dir, "vert", test, mainseed, size, self.loop_vert)
+    main(self.dir, "horiz", test, mainseed, size, self.loop_horiz)
+    main(self.dir, "fill", test, mainseed, size, self.loop_fill)
     return mainseed
 
 
