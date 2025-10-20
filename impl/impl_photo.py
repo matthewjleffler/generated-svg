ac0a3f782c7c0f8674f712f3bb6a3ef4f2cb5bf7
Matt Leffler <matthewjleffler@gmail.com>
2025-10-20 13:13:37 -0400

    Photo tweaks

--- impl/impl_photo.py
+++ impl/impl_photo.py
@@ -14,6 +14,7 @@ class PhotoParams(
   debug_draw_boundary: bool
   debug_draw_points: bool
 
+
 def CreateParams(defaults: Defaults) -> 'PhotoParams':
   result: PhotoParams = {
     'draw': True,
@@ -21,44 +22,6 @@ def CreateParams(defaults: Defaults) -> 'PhotoParams':
     'debug_draw_points': False,
   }
   return apply_defaults(result, defaults)
-
-# TODOML lib
-# def _get_clip_line(
-#     p0:Point,
-#     p1:Point,
-#     clip:shapely.geometry.Polygon,
-#     group: Group
-# ) -> List[List[Point]]:
-#   # extra: List[Point] = []
-#   # result: List[List[Point]] = []
-#   line = [p0, p1]
-
-#   return poly_intersect(p0, p1, clip)
-
-  # for poly in clip:
-  #   intersection =
-  #   print(intersection)
-
-  #   line = intersection[0]
-  #   for i in range(1, len(intersection)):
-  #     extra.append(intersection[i])
-  #   if len(line) < 1:
-  #     break
-
-  # if len(line) > 0:
-  #   result.append(line)
-  #   draw_point_path(line, group)
-  # for ext in extra:
-  #   extra_lines = _get_clip_line(ext[0], ext[1], clip, group)
-  #   if len(extra_lines) > 0:
-  #     for extra_line in extra_lines:
-  #       result.append(extra_line)
-
-  # return result
-
-# def _draw_clip_path(path: List[Point], clip:List[shapely.geometry.Polygon], group: Group):
-#   for i in range(0, len(path) - 1):
-#     _draw_clip_line(path[i], path[i + 1], clip, group)
 
 
 def draw_photo(params: PhotoParams, group: Group, seed: int):
@@ -70,20 +33,42 @@ def draw_photo(params: PhotoParams, group: Group, seed: int):
   if params['debug_draw_boundary']:
     draw_border(group)
 
-  delta = 0.03 #TODOML how much?
-  threshold = 0.4 # Beyond this range, don't bother drawing the shape
+  delta = 0.045 #TODOML how much?
+  threshold = 0.65 # Beyond this range, don't bother drawing the shape
   draw_step_min = 1
-  draw_step_max = 5
+  draw_step_max = 3
   clamp_value = 100
   min_bounds_size = 5 # Don't draw shapes that are less than this many pixels on a side
+  do_rotate = True
+  inset = True
+  inset_amount = 2
+  min_draw_length = 2
+  do_simplify = True
+  simplify_amount = 8
+  do_overlap = True
 
-  # TODOML image path?
   # Find all the pixel darknesses
   width = 0
   height = 0
+  rotated = False
   pixel_values: dict[tuple[int, int], float] = dict()
-  with Image.open('./photo/unnamed.jpg') as im:
+  # TODOML image path?
+  with Image.open('./photo/burrito.jpeg') as im:
     (width, height) = im.size
+
+    # Rotate to fit best
+    if do_rotate and height > width:
+      print("Rotating to fit")
+      rotated = True
+      im = im.rotate(90, expand=1)
+      (width, height) = im.size
+
+    # Resize to fit
+    (offset, scale) = scale_rect_to_fit(Rect(0, 0, width, height), pad)
+    im = im.resize((int(width * scale), int(height * scale)))
+    (width, height) = im.size
+
+    # Get pixel darknesses
     for y in range(0, height):
       for x in range(0, width):
         (r, g, b) = im.getpixel((x, y))
@@ -103,7 +88,7 @@ def draw_photo(params: PhotoParams, group: Group, seed: int):
       cur_avg = pixel_values[start_key]
       clamped_avg = floor(cur_avg * clamp_value) / clamp_value
 
-      if clamped_avg >= 1 - threshold:
+      if clamped_avg >= threshold:
         # Don't include, too bright
         continue
 
@@ -125,7 +110,7 @@ def draw_photo(params: PhotoParams, group: Group, seed: int):
           continue
 
         # In group, append and delete from points
-        cur_group.append(Position(offset.x + next.x * scale, offset.y + next.y * scale, clamped_avg))
+        cur_group.append(Position(offset.x + next.x, offset.y + next.y, clamped_avg / threshold))
         pixel_values.pop(next_key)
 
         for (o_x, o_y) in neighbors:
@@ -138,7 +123,6 @@ def draw_photo(params: PhotoParams, group: Group, seed: int):
           # This is a valid neighbor, add it to edge
           edges.append(Point(n_x, n_y))
 
-
   # Make polygons from each group
   group_len = len(sort_groups)
 
@@ -149,15 +133,15 @@ def draw_photo(params: PhotoParams, group: Group, seed: int):
     points: List[tuple[float, float]] = []
 
     print_overwrite(f"Processing shape {pad_max(i + 1, group_len)}")
-    x_min = width + 100
-    y_min = height + 100
+    x_min = maxsize
+    y_min = maxsize
     x_max = 0
     y_max = 0
     for pos in shape:
-      x_min = min(pos.x, x_min)
-      y_min = min(pos.y, y_min)
-      x_max = max(pos.x, x_max)
-      y_max = max(pos.y, y_max)
+      x_min = int(min(pos.x, x_min))
+      y_min = int(min(pos.y, y_min))
+      x_max = int(max(pos.x, x_max))
+      y_max = int(max(pos.y, y_max))
       points.append((pos.x, pos.y))
 
     if x_min == x_max or y_min == y_max:
@@ -170,36 +154,62 @@ def draw_photo(params: PhotoParams, group: Group, seed: int):
     if hull.geom_type == 'Polygon':
       hull = shapely.MultiPolygon([hull])
 
-    for polygon in hull.geoms:
-      (minx, miny, maxx, maxy) = polygon.bounds
-      if maxx - minx < min_bounds_size or maxy - miny < min_bounds_size:
-        # Too small to draw
-        continue
-      all_polygons.append(polygon)
-      polygon_density.append(shape[0].size)
+    if hull.geom_type == 'MultiPolygon':
+      for polygon in hull.geoms:
+        (minx, miny, maxx, maxy) = polygon.bounds
+        if maxx - minx < min_bounds_size or maxy - miny < min_bounds_size:
+          # Too small to draw
+          continue
+        all_polygons.append(polygon)
+        polygon_density.append(shape[0].size)
 
   print_finish_overwite()
 
   all_len = len(all_polygons)
   print(f"Finished processing shapes, found {all_len} polygons")
 
-  # Process polygons
-  for i in range(all_len):
-    first_polygon = all_polygons[i]
-    print_overwrite(f"Finding overlaps {pad_max(i + 1, all_len)}")
 
-    for j in range(all_len):
-      second_polygon = all_polygons[j]
-      if first_polygon == second_polygon:
-        continue
+  # Simplify polygons
+  if do_simplify:
+    for i in range(all_len):
+      polygon = all_polygons[i]
+      print_overwrite(f"Simplifying {pad_max(i + 1, all_len)}")
 
-      if first_polygon.contains(second_polygon):
-        all_polygons[i] = first_polygon - second_polygon
-      elif second_polygon.contains(first_polygon):
-        all_polygons[j] = second_polygon - first_polygon
+      # all_polygons[i] = shapely.simplify(polygon, simplify_amount)
+      all_polygons[i] = polygon.buffer(simplify_amount).buffer(-simplify_amount)
 
-  print_finish_overwite()
-  print("Finished finding overlaps")
+    print_finish_overwite();
+    print("Finished simplifying")
+
+
+  # Find overlaps
+  if do_overlap:
+    for i in range(all_len):
+      first_polygon = all_polygons[i]
+      print_overwrite(f"Finding overlaps {pad_max(i + 1, all_len)}")
+
+      for j in range(all_len):
+        second_polygon = all_polygons[j]
+        if first_polygon == second_polygon:
+          continue
+
+        if first_polygon.contains(second_polygon):
+          # all_polygons[i] = first_polygon - second_polygon
+          pass
+        elif second_polygon.contains(first_polygon):
+          all_polygons[j] = second_polygon - first_polygon
+          # pass
+        elif first_polygon.overlaps(second_polygon):
+          first_area = first_polygon.area
+          second_area = second_polygon.area
+          if first_area > second_area:
+            all_polygons[i] = first_polygon - second_polygon
+          else:
+            all_polygons[j] = second_polygon - first_polygon
+
+    print_finish_overwite()
+    print("Finished finding overlaps")
+
 
   # Draw lines
   draw_lines: List[List[Point]] = []
@@ -208,19 +218,40 @@ def draw_photo(params: PhotoParams, group: Group, seed: int):
     polygon = all_polygons[i]
     density = polygon_density[i]
     (minx, miny, maxx, maxy) = polygon.bounds
-    draw_y = 0
-    while draw_y <= maxy:
-      lines = poly_intersect(Point(minx, draw_y), Point(maxx, draw_y), polygon)
+    draw_pos = 0
+    if rotated:
+      max_draw = maxx
+    else:
+      max_draw = maxy
+    while draw_pos <= max_draw:
+      if rotated:
+        lines = poly_intersect(Point(draw_pos, miny), Point(draw_pos, maxy), polygon)
+      else:
+        lines = poly_intersect(Point(minx, draw_pos), Point(maxx, draw_pos), polygon)
       for line in lines:
         if len(line) < 1:
           continue
+        if inset:
+          #TODOML inset out of bounds
+          if rotated:
+            line[0].y += inset_amount
+            line[1].y -= inset_amount
+          else:
+            line[0].x += inset_amount
+            line[1].x -= inset_amount
+        calc_line = Line(line[0], line[1])
+        if calc_line.length() < min_draw_length:
+          continue
         draw_lines.append(line)
-      draw_y += draw_step_min + draw_step_max * density
+      draw_pos += draw_step_min + draw_step_max * density
 
   print_finish_overwite()
 
   # Sort lines
-  draw_lines.sort(key=lambda x: (x[0].y, x[0].x))
+  if rotated:
+    draw_lines.sort(key=lambda x: (floor(x[0].x), x[0].y))
+  else:
+    draw_lines.sort(key=lambda x: (floor(x[0].y), x[0].x))
 
   # Draw sorted lines
   for line in draw_lines:
